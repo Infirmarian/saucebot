189f37275596bec1551ca9e9d9006fc71f96e2da
Robert Geil <robert@rgeil.net>
2018-10-19 20:37:21 -0700

    Updated sauce_boy to format text
    - Added text formatting for sauce_boy's information
    - Created a function to add a food item to the list of checking
    - Created a function to remove foods to check

--- sauce_boy.py
+++ sauce_boy.py
@@ -7,15 +7,52 @@ import csv
 
 def main():
     dining_list = [
-        "http://menu.dining.ucla.edu/Menus/Covel/yesterday"
+        ["Covel", "http://menu.dining.ucla.edu/Menus/Covel"],
+        ["De Neve", "http://menu.dining.ucla.edu/Menus/DeNeve"],
+        ["Bruin Plate", "http://menu.dining.ucla.edu/Menus/BruinPlate"]
     ]
+    full_h = {}
     for location in dining_list:
-        result = parse_page(location)
+        result = parse_page(location[1])
         for key in result:
             store_food_csv(result[key])
-    
+        full_h[location[0]] = result
     to_check = load_list_to_check()
-    print(to_check)
+    x = find_items(full_h, to_check)
+    print(format_text(x))
+
+def format_text(items_dict):
+    msg = ""
+    for item in items_dict:
+        for hall in (items_dict[item]):
+            msg += item+" is in "+hall+" at "+format_times(items_dict[item][hall])+"\n"
+
+    if len(msg) > 0:
+        msg = "Today in the dining halls:\n"+msg
+    else:
+        msg = "Sorry! None of your selected items are in any dining hall today!"
+    return msg
+
+def format_times(times):
+    if len(times)==1:
+        return times[0]
+    if len(times)==2:
+        return times[0]+" and "+times[1]
+    if len(times) == 3:
+        return times[0] + ", "+times[1]+" and "+times[2] 
+
+def find_items(full_menu, to_check):
+    locations = {}
+    for food in to_check:
+        locations[food] = {}
+        for hall in full_menu:
+            for meal in full_menu[hall]:
+                if food in full_menu[hall][meal]:
+                    if hall in locations[food]:
+                        locations[food][hall].append(meal)
+                    else:
+                        locations[food][hall] = [meal]
+    return locations
 
 
 def get_page_dom(url):
@@ -32,8 +69,31 @@ def load_list_to_check():
     else:
         with open("check.json", "r") as f:
             data = json.load(f)
-        return data.keys()  # Return all the keys of the dictionary
+        return list(data.keys())  # Return all the keys of the dictionary
 
+def add_food_item(item):
+    if not os.path.exists("check.json"):
+        with open("check.json", "w") as f:
+            json.dump({item:0}, f)
+    else:
+        with open("check.json", "r") as f:
+            data = json.load(f)
+        if item not in data:
+            data[item] = 0
+            with open("check.json", "w") as f:
+                json.dump(data, f)
+
+def remove_food_item(item):
+    if not os.path.exists("check.json"):
+        with open("check.json", "w") as f:
+            json.dump({}, f)
+        return 0
+    with open("check.json", "r") as f:
+        data = json.load(f)
+    existing = data.pop(item, None)
+    if existing is not None:
+        with open("check.json", "w") as f:
+            json.dump(data, f)
 
 def parse_page(url):
     text = get_page_dom(url)
@@ -43,12 +103,11 @@ def parse_page(url):
     meals = soup.select(".menu-block")
     return_val = {}
     for meal in meals:
-        name = meal.h3
+        name = meal.h3.text
         food_list = meal.select("a.recipelink")
         return_val[name] = []
         for food in food_list:
             return_val[name].append(food.text)
-            print(food.text)
     return return_val
 
 
@@ -89,5 +148,6 @@ def message_groupme(msg, img=None):
         print("Error, message wasn't sent")
 
 
-main()
+if __name__ == '__main__':
+    main()
 
