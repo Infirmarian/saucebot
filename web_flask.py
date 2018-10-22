3b7da85d06bd75d07b6965084231e3ffc2fcb2ec
Robert Geil <robert@rgeil.net>
2018-10-22 11:31:15 -0700

    Fixed bug where Bot responded to own messages

--- web_flask.py
+++ web_flask.py
@@ -10,7 +10,8 @@ def test():
 
 @app.route("/sauce", methods=["POST"])
 def sauce():
-    sauce_boy.parse_user_input(request.json["text"], group_id=request.json["group_id"])
+    if request.json["sender_type"] != "bot":
+        sauce_boy.parse_user_input(request.json["text"], group_id=request.json["group_id"])
     return "Thanks GroupMe!"
 
 if __name__ == "__main__":
