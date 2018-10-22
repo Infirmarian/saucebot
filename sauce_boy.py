01bcf8ebd8525c07b78461510194e3dd0ef5ee9f
Robert Geil <robert@rgeil.net>
2018-10-22 11:13:55 -0700

    Added group tags, more interaction
    - Added the group id, so multiple bots can exist in the same program
    - Added more interactions including adding items or giving a description of the bot
    - Added fuzzy logic for matching

--- sauce_boy.py
+++ sauce_boy.py
@@ -3,9 +3,55 @@ import requests
 import json
 import os
 import csv
+import time
+from fuzzywuzzy import fuzz
 
 
-def main():
+def parse_user_input(text, group_id):
+    # I wasn't mentioned! :(
+    if text.lower().find("!sauce bot") == -1:
+        return
+    if text.lower() == "!sauce bot info":
+        message_groupme("Hi, I'm Sauce Bot, a totally useless bot originally built \
+        to track White Sauce Pasta at Covel. I've since grown and you can have me \
+        check for other food items in the UCLA dining halls as well!\n\
+        You can get my attention by saying !Sauce Bot and I can do things like\
+        '!Sauce Bot list foods' to get a list of all the items I'm tracking\
+        or '!Sauce Bot add [food item here]' to give me another item to track", group_id)
+        return
+    if text.find("list") != -1:
+        message_groupme(get_items_tracked(group_id), group_id)
+        return
+    if text.find("track") != -1:
+        item = text[text.find("track ")+6:]
+        message_groupme(try_to_add(item, group_id), group_id)
+        return 
+    if text.find("add") != -1:
+        item = text[text.find("add ")+4:]
+        message_groupme(try_to_add(item, group_id), group_id)
+        return
+    message_groupme("Hi there, I'm just a new bot, and I don't know that command yet!", group_id)
+
+def try_to_add(item, group_id):
+    food_list = load_food_csv()
+    for food in food_list:
+        if food.lower() == item.lower():
+            return add_food_item(item, group_id)
+    suggestions = []
+    for food in food_list:
+        suggestions.append((food, fuzz.ratio(item, food)))
+    suggestions.sort(key= lambda tup: tup[1], reverse = True)
+    top_5 = suggestions[:5]
+    msg = ""
+    for sug in top_5:
+        if sug[1] > 50:
+            msg += "- "+sug[0]+"\n"
+    if len(msg) > 0:
+        return "Sorry, I couldn't find the item you wanted to add. Did you mean one of these?\n" +msg
+    return "I wasn't able to find any items close to what you asked for. I'm just a bot and I'm still gathering information, so check back later or try more accurate wording"
+
+
+def load_dining_pages():
     dining_list = [
         ["Covel", "http://menu.dining.ucla.edu/Menus/Covel"],
         ["De Neve", "http://menu.dining.ucla.edu/Menus/DeNeve"],
@@ -17,21 +63,13 @@ def main():
         for key in result:
             store_food_csv(result[key])
         full_h[location[0]] = result
-    to_check = load_list_to_check()
-    x = find_items(full_h, to_check)
-    print(format_text(x))
-
-def parse_user_input(text):
-    # I wasn't mentioned! :(
-    if text.find("!Sauce Bot") == -1 and text.find("!sauce bot") == -1:
-        return
-    if text.find("list") != -1:
-        message_groupme(get_items_tracked())
-    message_groupme("Hi there, I'm just a new bot, and I don't know that command yet!")
+        full_h["date"] = time.strftime(time.time(), "%Y %m %d")
+    with open("stored_menu.json", "w") as f:
+        json.dump(full_h, f)
 
 
-def get_items_tracked():
-    l = load_list_to_check()
+def get_items_tracked(group_id):
+    l = load_list_to_check(group_id)
     msg = "I'm currently tracking "
     if len(l) == 0:
         return "I'm not tracking any items! To add an item, say\n!Sauce Bot add [insert item here]"
@@ -40,11 +78,10 @@ def get_items_tracked():
     if len(l) == 2:
         return msg + l[0] +" and "+l[1]
     if len(l) > 2:
-        for i in range(len(l-1)):
+        for i in range(len(l)-1):
             msg += l[i]+", "
-        msg += "and "+l[:1]
+        msg += "and "+l[-1]
     return msg
-
 
 
 def format_text(items_dict):
@@ -88,26 +125,38 @@ def get_page_dom(url):
     print("Error, the page {} was unable to be reached, or no data was delivered.".format(url))
 
 
-def load_list_to_check():
+def load_list_to_check(group_id):
     if not os.path.exists("check.json"):
         with open("check.json", "w") as f:
-            json.dump({}, f)
+            json.dump({group_id:{}}, f)
+        return []
     else:
         with open("check.json", "r") as f:
             data = json.load(f)
-        return list(data.keys())  # Return all the keys of the dictionary
-
-def add_food_item(item):
-    if not os.path.exists("check.json"):
-        with open("check.json", "w") as f:
-            json.dump({item:0}, f)
-    else:
-        with open("check.json", "r") as f:
-            data = json.load(f)
-        if item not in data:
-            data[item] = 0
+        # Add group to check.json
+        if group_id not in data:
+            data[group_id] = {}
             with open("check.json", "w") as f:
                 json.dump(data, f)
+        return list(data[group_id].keys())  # Return all the keys of the dictionary
+
+def add_food_item(item, group_id):
+    if not os.path.exists("check.json"):
+        with open("check.json", "w") as f:
+            json.dump({group_id:{item:0}}, f)
+        return "Added {} to the list of items to check".format(item)
+    else:
+        with open("check.json", "r") as f:
+            data = json.load(f)
+        if group_id not in data:
+            data[group_id] = {item:0}
+        if item not in data[group_id]:
+            data[item] = 0
+        else:
+            return "{} was already being tracked!".format(item)
+        with open("check.json", "w") as f:
+            json.dump(data, f)
+        return "Added {} to the list of items to check".format(item)
 
 # A functionality to remove 
 def remove_food_item(item):
@@ -151,21 +200,30 @@ def store_food_csv(food_list):
         writer = csv.writer(f)
         for item in already_found_food:
             writer.writerow([item])
+
+def load_food_csv():
+    f_list = []
+    if os.path.exists("food_dict.csv"):
+        with open("food_dict.csv", "r") as f:
+            reader = csv.reader(f)
+            for line in reader:
+                f_list.append(line[0])
+    return f_list
     
 
 
-def get_bot_id():
+def get_bot_id(group_id):
     with open("bot_list.json", "r") as f:
         data = json.load(f)
-    if "bot_id" in data:
-        return data["bot_id"]
+    if group_id in data:
+        return data[group_id]
     print("Unable to load bot ID, exiting")
     return None
 
 
-def message_groupme(msg, img=None):
+def message_groupme(msg, group_id, img=None):
     data = {
-        "bot_id"  : get_bot_id(),
+        "bot_id"  : get_bot_id(group_id),
         "text"    : msg
     }
     response = requests.post("https://api.groupme.com/v3/bots/post", data=data)
@@ -175,6 +233,4 @@ def message_groupme(msg, img=None):
         print("Error, message wasn't sent")
 
 
-if __name__ == '__main__':
-    main()
 
--- web_flask.py
+++ web_flask.py
@@ -11,7 +11,7 @@ def test():
 @app.route("/sauce", methods=["POST"])
 def sauce():
     print("Got em!")
-    sauce_boy.parse_user_input(request.json["text"])
+    sauce_boy.parse_user_input(request.json["text"], group_id=request.json["group_id"])
     return "Thanks GroupMe!"
 
 if __name__ == "__main__":
